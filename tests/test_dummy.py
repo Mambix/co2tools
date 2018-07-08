11c3c4c2d64107de0da0e834f8467f1e70f2b79a
Mambix <ledi.mambix@gmail.com>
2018-07-08 12:31:34 +0100

    Add dummy test

---
+++ tests/test_dummy.py
@@ -0,0 +1,14 @@
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+import sys
+import unittest
+
+class TestDummy(unittest.TestCase):
+    def setUp(self):
+	self.held, sys.stdout = sys.stdout, StringIO()
+
+    def test_dummy(self):
+	self.assertTrue(True)
